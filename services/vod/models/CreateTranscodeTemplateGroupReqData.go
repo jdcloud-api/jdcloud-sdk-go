1ec3d63fd0236f743530b0a415efb637b5a22e57
Tanc009 <tancong@jd.com>
2021-06-25 02:33:05 +0000

    publish jdcloud-sdk-go version 1.1.12

--- services/vod/models/CreateTranscodeTemplateGroupReqData.go
+++ services/vod/models/CreateTranscodeTemplateGroupReqData.go
@@ -23,5 +23,5 @@ type CreateTranscodeTemplateGroupReqData struct {
     GroupName string `json:"groupName"`
 
     /*  (Optional) */
-    Templates GroupedTranscodeTemplateData `json:"templates"`
+    Templates []GroupedTranscodeTemplateData `json:"templates"`
 }
